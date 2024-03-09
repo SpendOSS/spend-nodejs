f79ed7f01e186fa235fbd9feff56bf89eb58797c
jagandvs <dvsjmr9@gmail.com>
2024-03-09 05:58:57 +0530

    added config change

--- database/admin.js
+++ database/admin.js
@@ -1,5 +1,7 @@
 let admin = require("firebase-admin");
-let serviceAccount = require("./adminsdk.json");
+let serviceAccount = require("/etc/secrets/adminsdk.json");
+// let serviceAccount = require("./adminsdk.json");
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
